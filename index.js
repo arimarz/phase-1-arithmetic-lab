cd29d3155eea8db89447ffebd042871ec5ff08c0
Ari <aritheriver@gmail.com>
2023-01-12 16:31:47 -0800

    finished

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-
-// Write your code here
+// function multiply(num1, num2) {
+//     return (num1 * num2);
+// };
+// function random() {
+//     return Math.floor(Math.random()+ 1);
+//     };
+// function mod(a, b) {
+//     return a%b;
+// };
+// function max(){
+//     return Math.max();
+// };
+const num1 = 2;
+const num2 = 31;
+const num3 = 6;
+const num4 = 10;
+const multiply = num1 * num2;
+console.log(multiply);
+const random = Math.floor(Math.random()+1);
+const mod = num4%num3;
+const max = Math.max(20,10,11);
+console.log(max);
